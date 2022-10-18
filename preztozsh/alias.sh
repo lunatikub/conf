73ce3138c08f2e4fa7bdcaaef2f28b99f68f6d76
Thomas Joly <thomas.joly@ledger.fr>
2022-10-18 10:46:44 +0200

    big clean

--- preztozsh/alias.sh
+++
@@ -1,2 +0,0 @@
-# alias
-alias 'e=emacs'
--- preztozsh/bindkey.sh
+++
@@ -1,7 +0,0 @@
-# Ctrl -> <-
-bindkey '^[[1;5D' backward-word
-bindkey '^[[1;5C' forward-word
-bindkey '^H' backward-kill-word
-bindkey '^[[3^' kill-word
-bindkey '^[[7~' beginning-of-line
-bindkey '^[[8~' end-of-line
