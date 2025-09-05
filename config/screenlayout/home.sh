f15c5a2a657411a7b565cde39895bb086c722b2b
lunatikub <joly.th@gmail.com>
2025-09-05 18:01:31 +0200

    add screenlayout

---
+++ config/screenlayout/home.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+xrandr --output eDP-1 --off --output HDMI-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
+
+feh --bg-scale /home/tjoly/ledger/background/11.jpeg
